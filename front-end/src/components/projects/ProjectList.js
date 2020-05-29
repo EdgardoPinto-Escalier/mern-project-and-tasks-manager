5ddf97d9a4c96cbb7d68ac2024bef0d31ca83f20
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-05-29 20:06:45 +0200

    Created the ProjectList component

---
+++ front-end/src/components/projects/ProjectList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Project from './Project';
+
+const ProjectList = () => {
+
+  const projects = [
+    {name: 'Laravel Site'},
+    {name: 'React Interface'},
+    {name: 'FlexBox Gallery'}
+  ]
+
+  return (
+    <ul className="project-list">
+      {projects.map(project => (
+        <Project
+          project={project}
+        />
+      ))}
+    </ul>
+  )
+}
+
+export default ProjectList;
