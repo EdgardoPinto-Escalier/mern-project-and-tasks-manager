fd5392ab671b61f3c02edf81373b5b81d99eac10
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-06-15 11:16:07 +0200

    Created express server

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,12 @@
-console.log('from index.js');
+const express = require('express');
+
+// First we create the server
+const app = express();
+
+// App's port
+const PORT = process.env.PORT || 4000;
+
+// Start application
+app.listen(PORT, () => {
+  console.log(`The server is working on port number ${PORT}`);
+});
