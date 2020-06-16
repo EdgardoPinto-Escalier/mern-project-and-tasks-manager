8fbbd9bc969861cb5c4b73d165d7308baae46936
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-06-16 11:41:16 +0200

    Added new functionality to successfully insert new users in the database

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,5 +1,16 @@
+const User = require('../models/User');
 
-
-exports.createUser = (req, res) => {
-  console.log(req.body);
+exports.createUser = async (req, res) => {
+  try {
+      let user;
+      // Create the new user
+      user = new User(req.body);
+      // Save the new user
+      await user.save();
+      // Confirmation message
+      res.send('The user has been created successfully!');
+  } catch (error) {
+      console.log(error);
+      res.status(400).send('Something went wrong');
+  }
 }
