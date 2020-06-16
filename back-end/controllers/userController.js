763d9503b580d9f5ed16615878fd3c69e17dae93
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-06-16 12:03:30 +0200

    Added a small check so the same user is not registered twice in the DB

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,14 +1,27 @@
 const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
+
+  // Extract email and password
+  const { email, password } = req.body;
+
   try {
-      let user;
+      // Check so the registered user is unique
+      let user = await User.findOne({ email });
+
+      if(user) {
+        return res.status(400).json({ msg: 'The user already exists in the Database'});
+      }
+
       // Create the new user
       user = new User(req.body);
+
       // Save the new user
       await user.save();
+
       // Confirmation message
       res.send('The user has been created successfully!');
+
   } catch (error) {
       console.log(error);
       res.status(400).send('Something went wrong');
