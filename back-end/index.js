e4d92cad66f44e499c32140d8c966c54a271b48b
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-06-16 09:33:54 +0200

    Added new configuration for the users routes

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 // First we create the server
 const app = express();
 
+// Then we connect to the DB
+connectDB();
+
 // App's port
 const PORT = process.env.PORT || 4000;
+
+// Import routes
+app.use('/api/users', require('./routes/users'));
 
 // Start application
 app.listen(PORT, () => {
---
+++ back-end/routes/users.js
@@ -0,0 +1,10 @@
+// Routes for user creation
+const express = require('express');
+const router = express.Router();
+
+// Create an user
+// api/users
+router.post('/', () => {
+
+});
+module.exports = router;
