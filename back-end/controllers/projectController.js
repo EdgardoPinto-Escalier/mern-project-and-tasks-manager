6d7b71336c7593184e9df8334d0800632bf09201
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-06-20 07:45:59 +0200

    Added project validation

--- back-end/controllers/projectController.js
+++ back-end/controllers/projectController.js
@@ -1,13 +1,21 @@
 const Project = require('../models/Project');
+const { validationResult } = require('express-validator');
 
 exports.createProject = async (req, res) => {
+
+  // Check if there are any errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
   try {
     // Create a new project
     const project = new Project(req.body);
 
     // Save project creator via JWT
     project.createdBy = req.user.id;
-    
+
     // Save project
     project.save();
     res.json(project);
--- back-end/routes/projects.js
+++ back-end/routes/projects.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
 const authentication = require('../middleware/authentication');
+const { check } = require('express-validator');
 
 // Create projects
 // api/projects
 router.post('/',
   authentication,
+  [
+    check('name', 'The project name is required').not().isEmpty()
+  ],
   projectController.createProject
 );
 
