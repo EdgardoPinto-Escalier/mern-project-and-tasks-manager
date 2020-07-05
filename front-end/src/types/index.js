fea6457b779757a5cfdfeb97248a6c445c0b41ae
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-07-05 21:42:12 +0200

    Added new types

--- front-end/src/types/index.js
+++ front-end/src/types/index.js
@@ -16,3 +16,10 @@ export const CLEAN_TASK = 'CLEAN_TASK';
 
 export const SHOW_ALERT = 'SHOW_ALERT';
 export const HIDE_ALERT = 'HIDE_ALERT';
+
+export const SUCCESSFULL_REGISTER = 'SUCCESSFULL_REGISTER';
+export const ERROR_REGISTER = 'ERROR_REGISTER';
+export const GET_USER = 'GET_USER';
+export const SUCCESSFULL_LOGIN = 'SUCCESSFULL_LOGIN';
+export const ERROR_LOGIN = 'ERROR_LOGIN';
+export const CLOSE_SESSION = 'CLOSE_SESSION';
